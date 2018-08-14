f34919d089d17a99940a2c4ed4bab09fd3e77f3a
Victor Koenders <victor.koenders@gmail.com>
2018-08-14 18:35:50 +0200

    Ran cargo fmt

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,7 @@
-use mio::{Poll, Token, Ready, PollOpt, Events, Evented};
+use mio::{Evented, Events, Poll, PollOpt, Ready, Token};
 use std::process::{Command, Stdio};
-use ::{CommandAsync, ProcessEvent};
 use std::sync::mpsc::TryRecvError;
+use {CommandAsync, ProcessEvent};
 
 #[test]
 fn test_ping() {
@@ -9,7 +9,8 @@ fn test_ping() {
     if cfg!(target_os = "linux") {
         process.arg("-c").arg("4");
     }
-    let mut process = process.arg("8.8.8.8")
+    let mut process = process
+        .arg("8.8.8.8")
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn_async()
@@ -17,7 +18,9 @@ fn test_ping() {
     let poll = Poll::new().expect("Could not spawn poll");
     let mut events = Events::with_capacity(10);
     let token = Token(1);
-    process.register(&poll, token, Ready::all(), PollOpt::edge()).expect("Could not register");
+    process
+        .register(&poll, token, Ready::all(), PollOpt::edge())
+        .expect("Could not register");
     'outer: loop {
         poll.poll(&mut events, None).expect("Could not poll");
         for event in &events {
@@ -34,10 +37,10 @@ fn test_ping() {
                     ProcessEvent::Exit(exit_status) => {
                         assert!(exit_status.success());
                         break 'outer;
-                    },
+                    }
                     ProcessEvent::IoError(_, _) | ProcessEvent::CommandError(_) => {
                         assert!(false);
-                    },
+                    }
                     _ => {}
                 }
             }
@@ -49,25 +52,32 @@ fn test_ping() {
 pub mod windows {
     extern crate winapi;
     use std::process::{Command, Stdio};
-    use ::CommandAsync;
+    use CommandAsync;
 
     #[test]
-    fn test_terminate(){
+    fn test_terminate() {
         let mut process = Command::new("ping");
-        let mut process = process.arg("8.8.8.8")
+        let mut process = process
+            .arg("8.8.8.8")
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
             .spawn_async()
             .expect("Could not spawn process");
 
         let pid = process.id();
-        assert!(process_is_running(pid), "Expected process to run after starting it");
+        assert!(
+            process_is_running(pid),
+            "Expected process to run after starting it"
+        );
         process.kill().expect("Could not kill process");
-        assert!(process_is_running(pid), "Expected process to be terminated after terminating it");
+        assert!(
+            process_is_running(pid),
+            "Expected process to be terminated after terminating it"
+        );
     }
 
     fn process_is_running(pid: u32) -> bool {
-        let handle = unsafe {winapi::um::processthreadsapi::OpenProcess(0x0001, 1, pid) };
+        let handle = unsafe { winapi::um::processthreadsapi::OpenProcess(0x0001, 1, pid) };
         !handle.is_null()
     }
 }
@@ -75,7 +85,7 @@ pub mod windows {
 #[cfg(target_os = "unix")]
 pub mod unix {
     #[test]
-    fn test_terminate(){
+    fn test_terminate() {
         unimplemented!();
     }
 }
